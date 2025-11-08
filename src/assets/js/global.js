895617bb8ac63f25b970ef5d23f2fcacb64fa89b
vanessasickles <vsicklesshs@gmail.com>
2025-11-08 00:08:21 -0600

    feat(experience): Update experience & highlight tech in experience on hover

--- src/assets/js/global.js
+++ src/assets/js/global.js
@@ -1,4 +1,6 @@
-import { annotate } from 'rough-notation';
+import { annotate, annotationGroup } from 'rough-notation';
+
+// to-do: enable pre-annotated items via query string
 
 function classify(string) {
     return string.replace(/[., ]/g, '-').toLowerCase() 
@@ -17,6 +19,19 @@ projectElements.forEach(el => {
     }
 })
 
+const workplaceElements = document.querySelectorAll('.workplace .work-description li')
+const workplaceTechExamples = []
+
+workplaceElements.forEach(el => {
+    if (el.dataset.tech) {
+        workplaceTechExamples.push({
+            element: el,
+            tech: el.dataset.tech.split(',')
+        })
+    }
+})
+
+
 // Items to underline on hover
 const hoverSelectors = [
     '#technologies-list button',
@@ -25,7 +40,7 @@ const hoverSelectors = [
 ]
 const hoverSelectorsString = hoverSelectors.join(",")
 const elementsToAnimateOnhover = document.querySelectorAll(hoverSelectorsString)
-const defaultUnderlineSettings = { type: 'underline', color: 'var(--color-blue)', animationDuration: 300, strokeWidth: 6, padding: 0 }
+const defaultUnderlineSettings = { type: 'underline', color: 'var(--color-blue)', animationDuration: 250, strokeWidth: 6, padding: 0 }
 const defaultBoxSettings = { type: 'box', color: 'var(--color-blue)', animationDuration: 700, strokeWidth: 6, padding: 16 }
 
 elementsToAnimateOnhover.forEach(el => {
@@ -41,19 +56,39 @@ elementsToAnimateOnhover.forEach(el => {
         const isTechnologyButton = el.parentNode.parentNode.getAttribute("id") === 'technologies-list'
         if (isTechnologyButton) {
             const techType = el.innerHTML
+
+            const allTechExamplesAnnotations = []
+
+            // Highlight projects that use this tech
             for (const projectKey in projectTechData) {
                 const project = projectTechData[projectKey]
                 if (project.tech.includes(techType)) {
                     // project includes tech; underline it
-
                     const projectAnnotation = annotate(project.element, defaultBoxSettings)
-                    projectAnnotation.show()
+                    // projectAnnotation.show()
+                    allTechExamplesAnnotations.push(projectAnnotation)
 
                     el.addEventListener('mouseleave', () => {
                         projectAnnotation.remove()
                     })
                 }
             }
+
+            // Highlight experience example that uses this tech
+            workplaceTechExamples.forEach(workplaceItem => {
+                if (workplaceItem.tech.includes(techType)) {
+                    const workplaceItemAnnotation = annotate(workplaceItem.element, defaultUnderlineSettings)
+                    // workplaceItemAnnotation.show()
+                    allTechExamplesAnnotations.push(workplaceItemAnnotation)
+
+                    el.addEventListener('mouseleave', () => {
+                        workplaceItemAnnotation.remove()
+                    })
+                }
+            })
+
+            const techAnnotationGroup = annotationGroup(allTechExamplesAnnotations)
+            techAnnotationGroup.show()
         }
     })
 })
